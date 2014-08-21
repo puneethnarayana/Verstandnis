954e32c857f40a8fc5be21578be9cfe9eeb9bd14
N Puneeth <puneeth97@gmail.com>
2014-08-21 08:05:01 +0530

    Using MERGE inside STORED PROC

---
+++ SQL Server/Proc_LoadCity_v3.sql
@@ -0,0 +1,88 @@
+USE [TrainingDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_load_city_v3]    Script Date: 08/20/2014 16:21:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+ALTER PROC [dbo].[usp_load_city_v3]
+
+AS 
+BEGIN
+
+IF EXISTS (SELECT * FROM sysobjects WHERE name  = 'ARCHIVE')
+BEGIN
+DROP TABLE ARCHIVE
+END
+
+CREATE TABLE ARCHIVE
+(
+	[actions] varchar(10)
+	
+	,[City_ID] int
+	,[City_Code] varchar(15)
+	,[City_Name] varchar(15)
+	,[Population_Type] varchar(20)
+	,[country_id] int
+	
+	,[D_City_ID] int
+	,[D_City_Code] varchar(15)
+	,[D_City_Name] varchar(15)
+	,[D_Population_Type] varchar(20)
+	,[D_country_id] int
+)
+
+DECLARE @a varchar
+DECLARE @b varchar
+
+EXEC [TrainingDB].[dbo].[usp_load_country] @a, @b
+
+MERGE [dbo].[Dim_City] [DCity]
+USING
+(
+SELECT [SCCM].[city_code]
+,[SCCM].[city_name]
+,[SCCM].[Country_Code]
+,[DC].[country_id]
+FROM [TrainingDB].[stage].[source_city_country_mapping] [SCCM]
+JOIN [TrainingDB].[dbo].[dim_country] [DC] 
+ON [SCCM].[country_code] = [DC].[country_code]
+) AS J
+
+ON (J.city_code = [DCity].city_code)
+
+WHEN MATCHED AND (DCity.[city_name] <> J.[city_name]
+OR	ISNULL([DCity].[country_id],'') <> ISNULL(J.[country_id],0))
+THEN UPDATE
+	SET [DCity].[city_name] = [J].[city_name]
+	, [DCity].[country_id] = J.country_id
+	
+WHEN NOT MATCHED 
+THEN INSERT(
+	[City_Code]
+	,[City_Name]
+)
+VALUES 
+(
+	J.[city_code]
+	,J.[city_name]
+)
+
+OUTPUT $action, inserted.*,deleted.* INTO dbo.ARCHIVE
+;
+
+SELECT * FROM dbo.ARCHIVE
+PRINT @@ROWCOUNT
+END
+
+
+
+GO
+
+
